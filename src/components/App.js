f1beec66b4ec1fb0ecae52f4a2a7745612197662
OlgaStarchenko <o9915530@gmail.com>
2026-01-07 15:13:59 +1000

    Finished working with the Add.js and Form.js components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,33 @@
-import { Component } from '../core/Component';
-import { Form } from './Form';
-import { List } from './List';
-import { ListItem } from './ListItem';
+import { Component } from "../core/Component";
+import { Form } from "./Form";
+import { List } from "./List";
+import { ListItem } from "./ListItem";
 
 export class App extends Component {
   setup(props) {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'app';
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "app";
 
-    // ...
-    
+    this.state = {
+      total: 0,
+      donates: [],
+    };
+
+    const $h1 = document.createElement("h1");
+    const $span = document.createElement("span");
+    $span.textContent = this.state.total;
+    this.$total = $span;
+
+    $h1.textContent = `Итого: $${$span.textContent}`;
+
     const donateForm = new Form();
     this.$rootElement.appendChild(donateForm.$rootElement);
     const donateList = new List();
     this.$rootElement.appendChild(donateList.$rootElement);
+
+    this.$rootElement.appendChild($h1);
   }
-  
+
   onItemCreate(amount) {
     // ...
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,58 @@
-import { Component } from '../core/Component';
+import { Component } from "../core/Component";
 
 export class Form extends Component {
   setup(props) {
-    this.$rootElement = document.createElement('form');
-    this.$rootElement.className = 'donate-form';
+    this.state = {
+      amount: "",
+    };
+    this.$rootElement = document.createElement("form");
+    this.$rootElement.className = "donate-form";
+    this.$rootElement.addEventListener("submit", this.handleSubmit.bind(this));
 
-    // ...
+    const label = document.createElement("label");
+    label.htmlFor = "enter_amount";
+    label.setAttribute("tupe", "number");
+    label.textContent = "Введите сумму в $";
+
+    const input = document.createElement("input");
+    input.id = "enter_amount";
+    this.$input = input;
+    this.$input.addEventListener("input", this.handleInput.bind(this));
+
+    const button = document.createElement("button");
+    button.textContent = "Задонатить";
+    this.$button = button;
+
+    this.$rootElement.appendChild(label);
+    this.$rootElement.appendChild(input);
+    this.$rootElement.appendChild(button);
   }
 
   handleInput(event) {
-    // ...
+    this.state.amount = event.target.value;
+
+    console.log(this.isValid);
+    if (this.isValid) {
+      this.$button.disabled = false;
+    } else {
+      this.$button.disabled = true;
+    }
   }
 
   handleSubmit(event) {
-    // ...
+    event.preventDefault();
+    if (this.isValid) {
+      console.log(this.state.amount);
+      this.state.amount = "";
+      this.$input.value = "";
+    }
+  }
+
+  get isValid() {
+    if (this.state.amount > 0 && this.state.amount < 101) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
